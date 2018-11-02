0b1d60db97032f6b2c0d80a046a7799f59b849c5
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-02 14:44:59 +0800

    Implement the index, show and new methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#Create
+  #Get the form
+    get 'tasks/new', to: 'tasks#new'
+  #Post the form
+    post 'tasks', to: 'tasks#create'
+
+#Read
+  #Show one task
+    get 'tasks/:id', to: 'tasks#show', as: :task
+  #Index all tasks
+    get 'tasks', to: 'tasks#index'
+
+
+#Update
+  #Get the form
+    get 'tasks/:id/edit', to: 'tasks#edit'
+  #Update the tasks
+    patch 'tasks/:id', to: 'tasks#update'
+
+#Delete
+  #Delete a task
+    delete 'tasks/:id', to: 'tasks#destroy'
 
 end
